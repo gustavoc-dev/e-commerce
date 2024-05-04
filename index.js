03356cc410acf936b7c375e564d45036e4e4242b
gustavoc-dev <guscdossantos@gmail.com>
2024-05-04 00:54:12 -0300

    04/05/24

--- index.js
+++ index.js
@@ -1,21 +1,4 @@
-//Carrinho de compras
-window.onload = function(){
-    let count = 1
-    document.getElementById('radio1-card').checked = true
-
-    setInterval(function(){
-    nextImage()
-    },6000)
-
-    function nextImage(){
-    count++
-    if(count>3){
-    count === 1
-    }
-    document.getElementById('radio-'+count +"-card").checked = true
-    }
-
-    const shopCart = document.getElementById('shop-cart')
+const shopCart = document.getElementById('shop-cart')
 const shopCartMenu = document.getElementById('shop-cart-menu')
 
 function createShopCartMenu(){
@@ -29,62 +12,39 @@ function shopcartMenu(){
 
 shopCart.addEventListener('click',shopcartMenu)
 
-//slide automatico banner
-let countBanner = 1
-document.getElementById('radio1-banner').checked = true
-
-setInterval( function(){
-    nextImageBanner()
-},4000)
-
-function nextImageBanner(){
-    countBanner++
-    if(countBanner>3){
-        countBanner = 1
-    }
-    document.getElementById('radio'+countBanner+'-banner').checked = true
-}
-
-//Lançamentos
 const carousel = document.querySelector(".arrives-itens"),
 firstImg = carousel.querySelectorAll("img")[0],
 arrowIcons = document.querySelectorAll(".arrives i");
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+
 const showHideIcons = () => {
-    // showing and hiding prev/next icon according to carousel scroll left value
-    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth;
     arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
     arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
 }
 arrowIcons.forEach(icon => {
     icon.addEventListener("click", () => {
-        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
-        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        let firstImgWidth = firstImg.clientWidth + 14;
         carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+        setTimeout(() => showHideIcons(), 60);
     });
 });
 const autoSlide = () => {
-    // if there is no image left to scroll then return from here
     if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
-    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    positionDiff = Math.abs(positionDiff);
     let firstImgWidth = firstImg.clientWidth + 14;
-    // getting difference value that needs to add or reduce from carousel left to take middle img center
     let valDifference = firstImgWidth - positionDiff;
-    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+    if(carousel.scrollLeft > prevScrollLeft) { 
         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
-    // if user is scrolling to the left
     carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
 }
 const dragStart = (e) => {
-    // updatating global variables value on mouse down event
     isDragStart = true;
     prevPageX = e.pageX || e.touches[0].pageX;
     prevScrollLeft = carousel.scrollLeft;
 }
 const dragging = (e) => {
-    // scrolling images/carousel to left according to mouse pointer
     if(!isDragStart) return;
     e.preventDefault();
     isDragging = true;
@@ -106,4 +66,76 @@ document.addEventListener("mousemove", dragging);
 carousel.addEventListener("touchmove", dragging);
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("touchend", dragStop);
+
+
+let count = 1
+document.getElementById('radio1').checked = true
+
+setInterval( function(){
+    nextImage()
+},4000)
+
+function nextImage(){
+    count++
+    if(count>3){
+        count = 1
+    }
+    document.getElementById('radio'+count).checked = true
 }
+
+const carouselCombo = document.querySelector(".combos-itens"),
+firstImgCombo = carouselCombo.querySelectorAll("img")[0],
+arrowIconsCombo = document.querySelectorAll(".combos i");
+
+// const showHideIconsCombos = () => {
+//     let scrollWidth = carouselCombo.scrollWidth - carouselCombo.clientWidth;
+//     arrowIconsCombo[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+//     arrowIconsCombo[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+// }
+arrowIconsCombo.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImgCombo.clientWidth + 14;
+        carouselCombo.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIconsCombos(), 60);
+    });
+});
+const autoSlideCombo = () => {
+    if(carouselCombo.scrollLeft - (carouselCombo.scrollWidth - carouselCombo.clientWidth) > -1 || carouselCombo.scrollLeft <= 0) return;
+    positionDiff = Math.abs(positionDiff);
+    let firstImgWidth = firstImgCombo.clientWidth + 14;
+    let valDifference = firstImgWidth - positionDiff;
+    if(carouselCombo.scrollLeft > prevScrollLeft) { 
+        return carouselCombo.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    carouselCombo.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+const dragStartCombo = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carouselCombo.scrollLeft;
+}
+const draggingCombo = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carouselCombo.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carouselCombo.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIconsCombos();
+}
+const dragStopCombo = () => {
+    isDragStart = false;
+    carouselCombo.classList.remove("dragging");
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlideCombo();
+}
+
+
+
+carouselCombo.addEventListener("mousedown", dragStartCombo);
+carouselCombo.addEventListener("touchstart", dragStartCombo);
+document.addEventListener("mousemove", draggingCombo);
+carouselCombo.addEventListener("touchmove", draggingCombo);
+document.addEventListener("mouseup", dragStopCombo);
+carouselCombo.addEventListener("touchend", dragStopCombo);
